36e6cd036aadb704f4021a1428e4e5e40db2c920
docker <docker@asrock.localnet.jp>
2021-06-05 21:52:22 +0900

    update docker-compose.yml and start.sh add hello.c

--- start.sh
+++ start.sh
@@ -3,10 +3,6 @@
 trap_TERM() {
     now=$(date "+%Y/%m/%d-%H:%M:%S")
     echo "[${now}]" SIGTERM ACCEPTED
-
-    for filename in .bash_history .viminfo .profile .bash_logout; do
-        rm -f /home/armer/${filename}
-    done
     exit 0
 }
 
